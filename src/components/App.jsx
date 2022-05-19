0f60c539403047e35b350fbc0c3b643a74d04ad3
Damian Soto <soto.damian02@gmail.com>
2022-05-18 22:52:36 -0400

    Se junto archivo action con count

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Count from "./Count";
-import Actions from './Actions'
 
 function App() {
+  
   return (
     <div>
       <Header />
       <Count />
-      <Actions />
     </div>
   );
 }
